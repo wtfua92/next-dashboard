24f4fcc836cd6d3b6e32179a825ff7f0d2734c53
Andrii Tynok <andrii.tynok@adevinta.com>
2023-12-13 15:24:20 +0100

    feat: auth functionality

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,23 +4,49 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { AuthError } from 'next-auth';
+import { signIn } from '@/auth';
 
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
+  customerId: z.string({
+    invalid_type_error: 'Please select a customer.',
+  }),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: 'Please enter an amount greater than $0' }),
   status: z.enum(['pending', 'paid']),
-  date: z.string(),
+  date: z.string({
+    invalid_type_error: 'Please select an invoice status',
+  }),
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
-  const { customerId, amount, status } = CreateInvoice.parse({
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
+
+export async function createInvoice(prevState: State, formData: FormData) {
+  const validatedFields = CreateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields, failed to create an invoice',
+    };
+  }
+
+  const { amount, status, customerId } = validatedFields.data;
 
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
@@ -68,5 +94,24 @@ export async function deleteInvoice(id: string) {
     redirect('/dashboard/invoices');
   } catch (e) {
     return { message: 'Database Error: failed to delete the invoice' };
+  }
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (e) {
+    if (e instanceof AuthError) {
+      switch (e.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong';
+      }
+    }
+    throw e;
   }
 }
